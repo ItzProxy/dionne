1dd3eb260713a2aa9db6751a857629d0939c0cf3
Dionne Pasion <dionne@vivvo.com>
2019-01-15 09:46:55 -0600

    File structure change and removed some unnecessary files

--- dionne-service/src/main/java/com/vivvo/userservice/core/Email/MailGun/EmailService.java
+++
@@ -1,6 +0,0 @@
-package com.vivvo.userservice.core.Email.MailGun;
-
-public interface EmailService {
-    void sendText(String from, String to, String subject, String body);
-    void sendHTML(String from, String to, String subject, String body);
-}
